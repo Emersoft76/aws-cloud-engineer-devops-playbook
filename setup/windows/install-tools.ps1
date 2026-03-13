f62154d85ea963d837ebe884b6d6264088d8d515
Emerson Maciel <59801743+Emersoft76@users.noreply.github.com>
2026-03-13 21:53:05 +0000

    Create install-tools.ps1

---
+++ setup/windows/install-tools.ps1
@@ -0,0 +1,19 @@
+Write-Host "Installing AWS DevOps tools..."
+
+winget install Git.Git
+
+winget install Amazon.AWSCLI
+
+winget install Hashicorp.Terraform
+
+winget install Docker.DockerDesktop
+
+winget install Kubernetes.kubectl
+
+winget install Helm.Helm
+
+winget install Python.Python.3
+
+winget install OpenJS.NodeJS
+
+Write-Host "Installation complete."
