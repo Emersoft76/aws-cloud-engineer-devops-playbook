358a2fe9f81ab82dfd1832eeb63dd13c0ddd2cc2
Emerson Maciel <59801743+Emersoft76@users.noreply.github.com>
2026-03-13 22:02:29 +0000

    Create main.tf

---
+++ iac/terraform/modules/iam/main.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "terraform_role" {
+
+  name = "terraform-role"
+
+  assume_role_policy = jsonencode({
+
+    Version = "2012-10-17"
+
+    Statement = [
+
+      {
+
+        Action = "sts:AssumeRole"
+
+        Principal = {
+
+          Service = "ec2.amazonaws.com"
+
+        }
+
+        Effect = "Allow"
+
+      }
+
+    ]
+
+  })
+
+}
